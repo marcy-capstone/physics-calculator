088a99aa44893ef998075b1f52719bdf6154eac4
mayayr115 <mayayr115@gmail.com>
2024-08-15 16:38:47 -0400

    Buttons for equations work

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,6 @@
 from server import app
 from flask_sqlalchemy import SQLAlchemy # type: ignore
-from datetime import datetime
+from datetime import datetime, timezone
 import os
 import psycopg2  # type: ignore
 
@@ -12,10 +12,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(100), unique=True)
-    date_joined = db.Column(db.Date, default=datetime.utcnow())
+    date_joined = db.Column(db.Date, default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return '<User %r>' % self.username
+    
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    equation = db.Column(db.String(50))
+    variable = db.Column(db.String(100), unique=True)
+    time_submitted = db.Column(db.Date, default=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return '<Result %r>' % self.equation
 
 with app.app_context():
     db.create_all()
