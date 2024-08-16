719b61f932788f041481e2aea2491d7a3c771dea
mayayr115 <mayayr115@gmail.com>
2024-08-16 16:18:36 -0400

    All Velocity Variables Centered

--- frontend/src/pages/variables/VdPage.jsx
+++ frontend/src/pages/variables/VdPage.jsx
@@ -36,36 +36,32 @@ export default function VdPage() {
         };
     return (
         <>
-            <h1 className='has-text-white has-text-weight-bold'>Solving for Displacement!</h1>
-            <h1 className='has-text-white has-text-weight-medium is-size-2 mb-3'>d = V•t</h1>
-            <div className="field is-horizontal">
+            <h1 className='has-text-white has-text-weight-bold is-flex is-justify-content-center'>Solving for Displacement!</h1>
+            <h1 className='has-text-white has-text-weight-medium is-size-2 mb-3 is-flex is-justify-content-center'>d = V•t</h1>
+            <div className='is-flex is-flex-direction-column'>
+            <div className="field is-horizontal is-flex is-justify-content-center">
                 <label className="label has-text-white ">V =</label>
-                    <div className="control">
-                        <input className="input column is-4 ml-2" type="number" value={V} onChange={handleVChange} onKeyDown={handleKeyPress} name="V" placeholder="V ="/>
-                </div>
+                        <input className="input column is-1 ml-2" type="number" value={V} onChange={handleVChange} onKeyDown={handleKeyPress} name="V" placeholder="V ="/>
             </div>
-            <div className="field is-horizontal">
+            <div className="field is-horizontal is-flex is-justify-content-center">
                     <label className="label has-text-white">d =</label>
-                    <div className="control">
-                        <input className="input column is-4 ml-2" type="number" placeholder="?" disabled/>
-                    </div>
+                        <input className="input column is-1 ml-1" type="number" placeholder="?" disabled/>
             </div>
-            <div className="field is-horizontal">
+            <div className="field is-horizontal is-flex is-justify-content-center">
                     <label className="label has-text-white">t =</label>
-                    <div className="control">
-                        <input className="input column is-4 ml-3" type="number" value={Vt} onChange={handleVtChange} onKeyDown={handleKeyPress} name="Vt" placeholder="t ="/>
-                    </div>
+                        <input className="input column is-1 ml-2" type="number" value={Vt} onChange={handleVtChange} onKeyDown={handleKeyPress} name="Vt" placeholder="t ="/>
                 </div>
                 <div className="field">
-                <div className="control">
+                <div className="control is-flex is-justify-content-center ml-5">
                     <button className="button is-link" onClick={calculateD}>Calculate d!</button>
                 </div>
             </div>
             {result !== null && (
-                <div className="field">
+                <div className="field is-flex is-justify-content-center ml-5">
                     <p className='has-text-white'>d = {result} m</p>
                 </div>
             )}
+            </div>
         </>
     )
 }
